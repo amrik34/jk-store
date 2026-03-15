4488fa8991a4c118adb76cd00078ddebfb63c4ab
amrik34 <amrik3412singh@gmail.com>
2026-03-15 08:20:17 +0530

    deploy error

--- src/pages/home/components/Categories.tsx
+++ src/pages/home/components/Categories.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Categories() {
   return (
     <section className="section categories">
--- src/pages/home/components/FeaturedProducts.tsx
+++ src/pages/home/components/FeaturedProducts.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function FeaturedProducts() {
   return (
     <div>
